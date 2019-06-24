a1c04a5ce55918d452015ba5426b87dd67ae674d
Le H Ngo <seiji@o2.pl>
2019-06-24 13:42:07 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
+
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get "/hello" do
+		erb :hello
+	end
 
+	get "/goodbye" do
+		erb :goodbye
+	end
+
+	get "/date" do
+		@time = Time.now.strftime("%A, %B %d, %Y")
+		erb :date
+	end
 end
